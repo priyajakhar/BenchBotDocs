f8b3ff943435b7b01a11cff61ac2e28818d79d6a
Priya Jakhar <80418210+priyajakhar@users.noreply.github.com>
2022-02-11 13:48:34 -0500

    Delete GUI directory

--- GUI/Development Files/4Basic dummy file tests/moveContForward.py
+++
@@ -1,1 +0,0 @@
-print("Move continuously forward")
--- GUI/Rpi Files/v0.5/SimpleGUI/start.py
+++
@@ -1,7 +0,0 @@
-import sys
-sys.path.append("..")
-from MachineMotion import *
-
-mm = MachineMotion(DEFAULT_IP_ADDRESS.usb_windows)
-mm.releaseEstop()
-mm.resetSystem()
