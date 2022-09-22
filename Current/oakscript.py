d3fcf1fe86a0ef8ededc625dfe4991a143ce9c68
Priya Jakhar <80418210+priyajakhar@users.noreply.github.com>
2022-09-21 23:22:56 -0400

    updates for disparity

--- Current/oakscript.py
+++ Current/oakscript.py
@@ -1,19 +1,32 @@
-# DEPTH
-stereo = pipeline.create(dai.node.StereoDepth)
+
+Depth = pipeline.create(dai.node.StereoDepth)
 xoutDepth = pipeline.create(dai.node.XLinkOut)
-xoutDepth.setStreamName("DEPTH")
+xoutDepth.setStreamName("Disparity")
 
-stereo.setDefaultProfilePreset(dai.node.StereoDepth.PresetMode.HIGH_DENSITY)
-# LR-check is required for depth alignment
-stereo.setLeftRightCheck(True)
-stereo.setDepthAlign(dai.CameraBoardSocket.RGB)
-
-
-queueNames.append("rgb")
-queueNames.append("disp")
+# Properties
+# Create a node that will produce the depth map (using disparity output as it's easier to visualize depth this way)
+depth.setDefaultProfilePreset(dai.node.StereoDepth.PresetMode.HIGH_DENSITY)
+# Options: MEDIAN_OFF, KERNEL_3x3, KERNEL_5x5, KERNEL_7x7 (default)
+depth.initialConfig.setMedianFilter(dai.MedianFilter.KERNEL_7x7)
+depth.setLeftRightCheck(True)
+depth.setExtendedDisparity(False)
+depth.setSubpixel(False)
 
 # Linking
-camRgb.isp.link(rgbOut.input)
-left.out.link(stereo.left)
-right.out.link(stereo.right)
-stereo.disparity.link(disparityOut.input)
+depth.disparity.link(xoutDepth.input)
+
+
+
+qDepth = device.getOutputQueue(name="Disparity", maxSize=4, blocking=False)
+inDisparity = qDepth.get()
+frame = inDisparity.getFrame()
+# Normalization for better visualization
+frame = (frame * (255 / depth.initialConfig.getMaxDisparity())).astype(np.uint8)
+cv2.imshow("Disparity", frame)
+
+
+
+
+
+# stereo.setDepthAlign(dai.CameraBoardSocket.RGB)
+# camRgb.isp.link(rgbOut.input)
