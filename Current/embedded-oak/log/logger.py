09cef64abc3a3f78808161bbd50e34fcab29929b
priyajakhar <priyaftmage@gmail.com>
2023-08-31 22:29:00 -0400

    logger latest changes

--- Current/embedded-oak/log/logger.py
+++ Current/embedded-oak/log/logger.py
@@ -8,131 +8,164 @@ import cv2
 from pathlib import Path
 from from_root import from_root, from_here
 import csv
-import os
-
-main_path = from_root('')
-sys.path.append(str(main_path))
-
+import os, sys
 from datetime import datetime
-
 import json
+# imports for image logging
+import piexif
+from fractions import Fraction
 
+'''
+    Camera Status Logging Function
+'''
+img_log_dir = from_here("images_log")
+cam_log_dir = from_here("camera_log")
+# img_log_dir = Path("images_log")
+# cam_log_dir = Path("camera_log")
 
-Path("images_log").mkdir(parents=True, exist_ok=True)
-
-fields = ['Timestamp', 'Connected', 'NotConnected']
+Path(img_log_dir).mkdir(parents=True, exist_ok=True)
+Path(cam_log_dir).mkdir(parents=True, exist_ok=True)
 
 # if csv file doesn't exist, create one
-cam_status_file = Path('camera_status.csv')
+cam_status_file = cam_log_dir / "camera_status.csv"
+fields = ['Timestamp', 'Connected', 'NotConnected']
 if not cam_status_file.exists():
+    print("Camera Status file doesn't exist")
     with open(cam_status_file, 'w', newline='') as file: 
         writer = csv.DictWriter(file, fieldnames = fields)
         writer.writeheader()
-    
-num_of_cameras = 3
-updates = 0
-
-def log_image(img, img_type, gps_tag):
-    date_obj = datetime.utcnow()
-    day = date_obj.date()
-    hour = date_obj.time().hour
-    minutes = date_obj.time().minute
-
-    fileName = f"{day}_{hour}:{minutes}_{img_type}.png"
-    cv2.imwrite(fileName, img)
 
 
-# writing current camera status to the csv file
-def update_camera_status():
+# writing current camera status to the csv file ( takes < 3 ms )
+def system_camera_status():
+    # st = time.time()
     camera_dict = [{'Timestamp': None, 'Connected': [], 'NotConnected': []} ]
-    
-    json_files = [cam_json for cam_json in os.listdir("images_log/") if cam_json.endswith('.json')]
+    json_files = [cam_json for cam_json in os.listdir(cam_log_dir) if cam_json.endswith('.json')]
     # print(json_files)
     
     for cam_file in json_files:
-        with open("images_log/" + cam_file) as json_file:
+        with open(cam_log_dir / cam_file) as json_file:
             data = json.load(json_file)
             if data['Status'] == 'Connected':
                 camera_dict[0]['Connected'].append(data['Camera_ID'])
             else:
                 camera_dict[0]['NotConnected'].append(data['Camera_ID'])
     camera_dict[0]['Timestamp'] = datetime.utcnow()
+    camera_dict[0]['Connected'].sort()
+    camera_dict[0]['NotConnected'].sort()
+    # print(camera_dict)
 
-    with open('camera_status.csv', 'a', newline='') as file: 
+    with open(cam_status_file, 'a', newline='') as file: 
         writer = csv.DictWriter(file, fieldnames = fields)
         writer.writerows(camera_dict)
+    # et = time.time()
+    # elapsed_time = (et - st)*1000
+    # print('CSV Execution time:', elapsed_time)
 
+    print("----- Current System Status -----")
+    print("Connected Cameras:", camera_dict[0]['Connected'])
+    print("Disconnected Cameras:", camera_dict[0]['NotConnected'])
+
+# takes around 3 ms max, mostly < 1 ms
 def camera_status_log(cam_id, status):
+    # st = time.time()
     camera_dict = {'Camera_ID': cam_id, 'Timestamp': str(datetime.utcnow()), 'Status': status}
-    fileName = f"images_log/Camera_{cam_id}.json"
+    fileName = f"{cam_log_dir}/Camera_{cam_id}.json"
     # print(fileName)
+
     with open(fileName, 'w') as fp:
         json.dump(camera_dict, fp)
+    # et = time.time()
+    # elapsed_time = (et - st)*1000
+    # print('Execution time:', elapsed_time)
     
 
-camera_status_log(1, 'Connected')
-camera_status_log(2, 'Connected')
-camera_status_log(3, 'Connected')
-camera_status_log(4, 'Connected')
-camera_status_log(5, 'Connected')
-update_camera_status()
-
-# from exif import Image
-# import piexif
-
-
-# exif_dict = piexif.load("Rgb.jpg")
-# print(exif_dict)
+# camera_status_log(1, 'Connected')
+# camera_status_log(2, 'Connected')
+# camera_status_log(3, 'NotConnected')
+# camera_status_log(4, 'Connected')
+# camera_status_log(5, 'Connected')
+# camera_status_log(6, 'NotConnected')
+# camera_status_log(7, 'NotConnected')
+# camera_status_log(8, 'Connected')
+# system_camera_status()
 
 
-# with open('cat.png', 'rb') as image_file:
-    # my_image = Image(image_file)
 
-# print(my_image.has_exif)
+'''
+    Images Logging function
+'''
+
+# if csv file doesn't exist, create one
+img_log_file = img_log_dir / "images_log.csv"
+i_fields = ['Timestamp', 'Image Name', 'GPS']
+if not img_log_file.exists():
+    print("Image log file doesn't exist")
+    with open(img_log_file, 'w', newline='') as file: 
+        writer = csv.DictWriter(file, fieldnames = i_fields)
+        writer.writeheader()
 
 
-# gps_ifd = {
-        # piexif.GPSIFD.GPSVersionID: (2, 0, 0, 0),
-        # piexif.GPSIFD.GPSAltitudeRef: 1,
-        # piexif.GPSIFD.GPSAltitude: change_to_rational(round(altitude)),
-        # piexif.GPSIFD.GPSLatitudeRef: lat_deg[3],
-        # piexif.GPSIFD.GPSLatitude: exiv_lat,
-        # piexif.GPSIFD.GPSLongitudeRef: lng_deg[3],
-        # piexif.GPSIFD.GPSLongitude: exiv_lng,
-        # piexif.GPSIFD.GPSDateStamp: gpsTime
+def log_image(img, img_type, gps_tag):
+    # save image in the image log directory
+    date_obj = datetime.utcnow()
+    day = date_obj.date()
+    hour = date_obj.time().hour
+    minutes = date_obj.time().minute
+    imgName = f"{day}_{hour}:{minutes}_{img_type}.jpg"
+    fileName = f"{img_log_dir}/{imgName}"
+    cv2.imwrite(fileName, img)
+
+    # add gps tags to the saved image
+    lat = dd_to_dms(gps_tag[0])
+    lng = dd_to_dms(gps_tag[1])
+    if gps_tag[0]<0:    lat_ref = 'S'
+    else:   lat_ref = 'N'
+    if gps_tag[1]<0:    lng_ref = 'W'
+    else:   lng_ref = 'E'
+
+    exiv_lat = (to_rational(lat[0]), to_rational(lat[1]), to_rational(lat[2]))
+    exiv_lng = (to_rational(lng[0]), to_rational(lng[1]), to_rational(lng[2]))
+    gps_ifd = {
+        piexif.GPSIFD.GPSVersionID: (2, 0, 0, 0),
+        piexif.GPSIFD.GPSLatitudeRef: lat_ref,
+        piexif.GPSIFD.GPSLatitude: exiv_lat,
+        piexif.GPSIFD.GPSLongitudeRef: lng_ref,
+        piexif.GPSIFD.GPSLongitude: exiv_lng,
+    }
+    exif_dict = {"GPS": gps_ifd}
+    exif_bytes = piexif.dump(exif_dict)
+    piexif.insert(exif_bytes, fileName)
     
-    # }
+    # log gps tag with image name in the csv file
+    img_dict = [{'Timestamp': datetime.utcnow(), 'Image Name': imgName, 'GPS': gps_tag} ]
+    with open(img_log_file, 'a', newline='') as file: 
+        writer = csv.DictWriter(file, fieldnames = i_fields)
+        writer.writerows(img_dict)
 
-    # exif_dict = {"GPS": gps_ifd}
-    # exif_bytes = piexif.dump(exif_dict)
-    # piexif.insert(exif_bytes, file_name)
-    
-# exif_bytes=piexif.dump(exif_dict)
-# im=Image.open("foo.jpg")
-# im.thumbnail((100,100),Image.ANTIALIAS)
-# im.save("out.jpg",exif=exif_bytes)
+'''
+convert decimal degree to degrees, munutes and seconds
+arguments: gps-coordinates latitude or longitude e.g. 35.773713
+return: gps-coordinates in DMS (35, 46, 25.3668)
+'''
+def dd_to_dms(coordinate):
+    coordinate = abs(coordinate)
+    deg = int(coordinate)
+    min_sec = (coordinate - deg) * 60
+    min = int(min_sec)
+    sec = round((min_sec - min) * 60, 5)
+    return (deg, min, sec)
 
-
-
-
-
+'''
+convert integer values to fractions which is the format acceptable by exif for degrees, minutes and seconds
+arguments: 25.3668
+return: (63417, 2500)
+'''
+def to_rational(val):
+    f = Fraction(str(val))
+    return (f.numerator, f.denominator)
   
 
-
-
-
-'''
-With Pillow
-
-fromPILimportImage importpiexif im=Image.open(filename) exif_dict=piexif.load(im.info["exif"]) #processimandexif_dict... w,h=im.size exif_dict["0th"][piexif.ImageIFD.XResolution]=(w,1) exif_dict["0th"][piexif.ImageIFD.YResolution]=(h,1) exif_bytes=piexif.dump(exif_dict) im.save(new_file,"jpeg",exif=exif_bytes)
-
-
-
-
-
-
-
-'''
 
     
 '''
@@ -158,4 +191,6 @@ piexif There are only just five functions.
 - *remove(filename)* - Remove exif from JPEG, or WebP.
 - *transplant(filename, filename)* - Transplant exif from JPEG to JPEG.
 
+# im = Image.open(fileName)
+# im.save(fileName, 'jpeg', exif=exif_bytes)
 '''
--- Current/embedded-oak/log/test_logger.py
+++ Current/embedded-oak/log/test_logger.py
@@ -8,39 +8,106 @@ import time
 import numpy as np
 
 
+'''
+    reading contents of csv file
+'''
+# import csv
+# import sys
+# cam_log = 'camera_log/camera_status.csv'
+# img_log = 'images_log/images_log.csv'
+# with open(img_log) as csv_file:
+#     csv_reader = csv.reader(csv_file, delimiter=',')
+#     for row in csv_reader:
+#         print(row)
+# sys.exit()
+
+
+'''
+    reading contents of json file
+'''
+# import json
+# import sys
+# f = open('camera_log/Camera_1.json')
+# data = json.load(f)
+# print(data)
+# f.close()
+# sys.exit()
+
+
+'''
+    testing image log function
+'''
+# Latitude : 35.773713
+# Longitude: -78.672968
+# Easting: 710325.92
+# Northing: 3961357.01
+# zone: 17S
+
 # Load the image
 # image = cv2.imread("cat.png")
-
 image = cv2.imread("Rgb.jpg")
 
-print( image.dtype.metadata )
+# print( image.dtype.metadata )
 
-# logger.log_image(image, "Color", "23452346246")
+logger.log_image(image, "Color", [35.773713, -78.672968])
 
 
-logger.camera_status_log(1, True)
-logger.camera_status_log(3, False)
-logger.camera_status_log(4, True)
+# def convert_ddtodms(dd_gps):
+#     lat = 45.543210
+# >>> deg = int(dd_gps)
+# >>> min = int((dd_gps-deg)*60)
+# >>> sec = ((dd_gps-deg)*60) - min
+# >>> print(deg, min, sec)
 
-time.sleep(1)
-logger.camera_status_log(2, True)
-logger.camera_status_log(5, True)
-logger.camera_status_log(6, True)
+# def make_image(self, img_data, gps_data):
+    
+#     picSaveName = picNameTimeString + ".jpg"
+#     print("save image ")
+#     picPath = os.path.join(output_dir, picSaveName)
+#     cv2.imwrite(picPath, cv_img, params)
 
-time.sleep(1)
-logger.camera_status_log(1, True)
-logger.camera_status_log(7, False)
-logger.camera_status_log(8, True)
-
-time.sleep(1)
-logger.camera_status_log(1, False)
-logger.camera_status_log(7, False)
-
+#     picGpsTimeFlt =self.rostime2floatSecs(gps_data.header.stamp)
+#     gpsTimeString = datetime.utcfromtimestamp(picGpsTimeFlt).strftime('%Y:%m:%d %H:%M:%S')
+#     self.set_gps_location(picPath, lat ...
 
 
 
 
 
 '''
+    testing camera log function
+'''
 
+# logger.camera_status_log(1, True)
+# logger.camera_status_log(3, False)
+# logger.camera_status_log(4, True)
+
+# time.sleep(1)
+# logger.camera_status_log(2, True)
+# logger.camera_status_log(5, True)
+# logger.camera_status_log(6, True)
+
+# time.sleep(1)
+# logger.camera_status_log(1, True)
+# logger.camera_status_log(7, False)
+# logger.camera_status_log(8, True)
+
+# time.sleep(1)
+# logger.camera_status_log(1, False)
+# logger.camera_status_log(7, False)
+
+
+# logger.camera_status_log(1, 'Connected')
+# logger.camera_status_log(2, 'Connected')
+# logger.camera_status_log(3, 'NotConnected')
+# logger.camera_status_log(4, 'Connected')
+# logger.camera_status_log(5, 'Connected')
+# logger.camera_status_log(6, 'NotConnected')
+# logger.camera_status_log(7, 'NotConnected')
+# logger.camera_status_log(8, 'Connected')
+# logger.system_camera_status()
+
+'''
+35 46 25.3668
+78 40 22.6848
 '''
